e8179cc941640b5ac7015fd431c85665f1bb9c83
yinwei9 <yinwei9@guahao.com>
2018-05-28 01:37:51 +0800

    nginx-gateway 初始化配置

---
+++ phase/access/open_api_server_access.lua
@@ -0,0 +1,12 @@
+---
+--- Open API接口访问控制
+--- Created by luochen.
+--- DateTime: 2018/5/28 1:29
+---
+
+-- 引入库
+local request_method = ngx.var.request_method;
+
+ngx.log(ngx.INFO,"request method = ",request_method);
+
+
