b0b94490aec95c91eb17323ca71ba8f1024864eb
yinwei9 <yinwei9@guahao.com>
2018-06-02 17:23:24 +0800

    请求参数处理

--- helper/arg_helper.lua
+++ helper/arg_helper.lua
@@ -4,6 +4,7 @@
 --- DateTime: 2018/6/2 12:33
 ---
 local table_util = require("example.util.table_util")
+local cjson = require("cjson")
 
 local _M = {}
 
@@ -13,11 +14,13 @@ local _M = {}
         -- POST 请求参数
         ngx.req.read_body()
         local post_args = ngx.req.get_post_args() or {}
+        ngx.log(ngx.INFO,"post_args："..cjson.encode(post_args));
 
         -- POST 某些请求参数放入URL中
         local url_args = ngx.req.get_uri_args() or {}
-
+        ngx.log(ngx.INFO,"url_args："..cjson.encode(url_args));
         local args = table_util.merge(url_args, post_args);
+
         return args
     end
 
