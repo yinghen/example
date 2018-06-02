b6a5f0227bc0e2e124ab4ad00b7a546a57435750
yinwei9 <yinwei9@guahao.com>
2018-06-02 12:55:54 +0800

    测试5

--- handler/request_handler.lua
+++ handler/request_handler.lua
@@ -3,13 +3,22 @@
 --- Created by luochen.
 --- DateTime: 2018/5/28 1:33
 ---
+local cjson = require("cjson")
 
 local _M = {}
 
+    M.get_uuid = function()
+        return os.date("%Y%m%d%H%M%S", os.time()) .. "-" .. (math.random(random_seed))
+    end
+
     _M.init = function ()
+
+        ngx.ctx.request_start = common_util.now()
+        ngx.ctx.uuid = _M.get_uuid()
 
         -- 获取客户端IP
         local ip = "1932.33.4.4.4"
+        ngx.ctx.client_ip = ip
 
         -- 获取请求头
         local request_header = require("example.helper.header_helper")
@@ -19,7 +28,9 @@ local _M = {}
         -- 获取请求参数
         local arg_helper = require("example.helper.arg_helper")
         local args = arg_helper.get_args();
+        ngx.ctx.args = args
 
+        ngx.log(ngx.INFO, "accept request! uuid=", ngx.ctx.uuid, ", client ip=", ngx.ctx.client_ip, " headers=", cjson.encode(headers), ", args=", cjson.encode(args))
     end
 
 return _M;
--- helper/arg_helper.lua
+++ helper/arg_helper.lua
@@ -3,19 +3,14 @@
 --- Created by luochen.
 --- DateTime: 2018/6/2 12:33
 ---
-local cjson = require("cjson")
-
 local _M = {}
 
     -- 获取请求参数
     _M.get_args = function()
-
+        -- POST 请求参数
         ngx.req.read_body()
         local post_args = ngx.req.get_post_args()
-        for k, v in pairs(post_args) do
-            ngx.say(k, ": ", v, "<br/>")
-        end
-
+        return post_args
     end
 
 return _M
