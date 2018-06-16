75ed93689cbfe7578a645faf94bfea37f37eac51
yinwei9 <yinwei9@guahao.com>
2018-06-16 11:23:38 +0800

    测试

--- phase/init/context_builder.lua
+++ phase/init/context_builder.lua
@@ -9,13 +9,15 @@ local cjson = require("cjson")
 local _context = {}
     _context.init = function(config_path)
         local config = config_loader.load(config_path)
-        ngx.log(ngx.INFO,"config_path = ",config_path,"  config=", cjson.encode(config))
+        ngx.log(ngx.INFO,"config_path = ",config_path)
 
         ------------------- 初始化guahao的上下文环境  -------------------
         -- 初始化缓存
-        local global_config = ngx.shared.global_config;
-        global_config:set("config", config)
-        global_config:set("redis_config", redis_config)
-        global_config:set("config_items", config_items)
+        --local global_config = ngx.shared.global_config;
+        --global_config:set("config", config)
+        --global_config:set("redis_config", redis_config)
+        --global_config:set("config_items", config_items)
+
+        math.randomseed(os.time())
     end
 return _context
