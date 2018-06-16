cbd28618e39a0c1c13545f286d94d03dbbf1fff4
yinwei9 <yinwei9@guahao.com>
2018-06-16 10:41:44 +0800

    配置路径错误

--- phase/init/init_master.lua
+++ phase/init/init_master.lua
@@ -9,7 +9,7 @@ local context_builder = require("example.phase.init.context_builder")
 
 local _M = {}
     _M.init = function()
-        local config_path = "/home/luochen/example/config/nginx-gateway.conf"
+        local config_path = "/home/luochen/example/conf/nginx-gateway.conf"
         context_builder.init(config_path);
     end
 return _M
